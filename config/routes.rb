f0bc8f7dcebeb35725c2ec6ad612e8e8fcd5309f
cheenwe <cheenwe@gmail.com>
2019-11-13 18:15:26 +0800

    接口修改

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   match "api/down_gfs" => "api#down", via: :get#[:get, :post]
-  match "api/down_gfs" => "api#down", via: :get#[:get, :post]
+  match "api/down_ok" => "api#down_ok", via: :get#[:get, :post]
   match "api/create_gfs" => "api#create_gfs", via: :get#[:get, :post]
 
 end
